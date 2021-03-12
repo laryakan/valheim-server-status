378b5fc2170c5936a917095dbc01b7121c1f2eb6
laryakan <laryakan@live.fr>
2021-03-12 19:09:36 +0100

    updated menu, discord log sender, server status genericity

--- setup.sh
+++ setup.sh
@@ -17,9 +17,13 @@ function replace_env_value() {
 function setup_value_prompt() {
   echo ""
 	echo "**$1**"
-	echo "current value : ${!2}"
-	echo "enter new value :"
+	echo -e "current value : ${!2}"
+	echo "enter new value ('n' to leave unchanged) :"
 	read NEWVALUE
+	if [ "$NEWVALUE" = 'n' ];then
+	  echo "value unchanged"
+	  return
+	fi
 	replace_env_value "$2" "$NEWVALUE"
 	echo "new value set : ${!2}"
 	echo ""
@@ -34,6 +38,9 @@ function show_config() {
 ##
 green='\e[32m'
 blue='\e[34m'
+cyan='\e[36m'
+yellow='\e[93m'
+magenta='\e[35m'
 clear='\e[0m'
 ColorGreen(){
 	echo -ne $green$1$clear
@@ -41,17 +48,42 @@ ColorGreen(){
 ColorBlue(){
 	echo -ne $blue$1$clear
 }
+ColorCyan(){
+	echo -ne $cyan$1$clear
+}
+ColorYellow(){
+	echo -ne $yellow$1$clear
+}
+ColorMagenta){
+	echo -ne $magenta$1$clear
+}
 
 menu(){
 echo -ne "
----------------------------------
-=== Valheim Server Monitoring ===
-$(ColorGreen '1)') Setup Valheim Server Directory
-$(ColorGreen '0)') Quit
-$(ColorBlue 'Choose an option:') "
+---------------------------------------
+=== Valheim Server Monitoring - VSM ===
+$(ColorGreen '1)') setup $(ColorYellow 'current') Valheim server directory
+$(ColorGreen '2)') setup $(ColorCyan 'wanted') Valheim server logs directory
+$(ColorGreen '3)') setup $(ColorCyan 'wanted') Valheim server status over HTTP port
+$(ColorGreen '4)') setup $(ColorYellow 'current') $(ColorMagenta 'Discord') webhook ID
+$(ColorGreen '5)') setup $(ColorYellow 'current') $(ColorMagenta 'Discord') webhook token
+$(ColorGreen '6)') setup $(ColorCyan 'wanted') last logs on your $(ColorMagenta 'Discord') channel
+$(ColorGreen '7)') force $(ColorMagenta 'Discord') webhook update
+$(ColorGreen '8)') setup $(ColorYellow 'current') $(ColorMagenta 'Discord') webhook status message id
+$(ColorGreen '9)') setup $(ColorYellow 'current') $(ColorMagenta 'Discord') webhook last logs message id
+$(ColorGreen '0)') quit
+$(ColorBlue 'choose an option:') "
         read a
         case $a in
-	        1) setup_value_prompt 'Valheim Server Directory' 'VHSERVERDIR' ; menu ;;
+	        1) setup_value_prompt 'where is your dedicated server ?' 'VHSERVERDIR' ; menu ;;
+	        2) setup_value_prompt 'where do you want to put logs ?' 'VALHEIMSERVERLOGSDIR' ; menu ;;
+	        3) setup_value_prompt 'on which port do you want to provide your server status ?' 'STATUSPORT' ; menu ;;
+	        4) setup_value_prompt "what's your $(ColorMagenta 'Discord') webhook id ?" 'WEBHOOKID' ; menu ;;
+	        5) setup_value_prompt "what's your $(ColorMagenta 'Discord') webhook token ?" 'WEBHOOKTOKEN' ; menu ;;
+	        6) setup_value_prompt "how many logs do you want on your $(ColorMagenta 'Discord') ? set '0' if you dont want any" 'SENDLASTLOGS' ; menu ;;
+	        7) $CWD/discord/update ; menu ;;
+	        8) setup_value_prompt "what's the $(ColorMagenta 'Discord') webhook status message id ?" 'STATUSMESSAGEID' ; menu ;;
+	        9) setup_value_prompt "what's the $(ColorMagenta 'Discord') webhook 'last logs' message id ?" 'LASTLOGMESSAGEID' ; menu ;;
 		0) exit 0 ;;
 		*) echo -e $red"Wrong option."$clear; WrongCommand;;
         esac
